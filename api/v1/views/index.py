fc3077c0cc5185799e7a13600a6e7e124d00b98b
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-24 10:06:00 -0500

    pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,9 @@
 MODULE NAME: index
 -------------------------------------------------------------------------------
 """
-
 from api.v1.views import app_views
 from flask import jsonify
+
 
 @app_views.route("/status", strict_slashes=False)
 def status():
