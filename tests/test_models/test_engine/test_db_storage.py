0eb7f68f3560bf3cfd0db8aa5a664761eac97bbf
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-25 14:13:55 -0500

    test dbstorange fixed

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -94,3 +94,11 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_db_storange_method_get(self):
+        """Test for get method"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_db_storange_method_count(self):
+        """Test fot count method"""
