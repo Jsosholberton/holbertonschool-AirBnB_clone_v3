043a07a4a999b9e45830ff86ed814b0a099e6a39
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-25 13:35:57 -0500

    fixed

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -90,8 +90,9 @@ def update_review(review_id):
         abort(400, "Not a JSON")
 
     for key, value in new_json.items():
-        if key not in ("id", "user_id", "place_id", "created_at" "updated_at"):
-            setattr(Review, key, value)
+        if key not in ("id", "user_id", "place_id",
+                       "created_at", "updated_at"):
+            setattr(review, key, value)
 
     review.save()
     return jsonify(review.to_dict()), 200
